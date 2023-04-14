062b0ac12ba010d9620db1dd82aedead86d14713
Andre Besner <besner.andre@gmail.com>
2023-04-13 21:17:22 -0400

    get data from both APIs ready to be customised and parsed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,7 @@ var searchForm = document.querySelector("#search-form")//whole form to search
 var inputEl = document.querySelector("#search-field")//this is gonna be the bar where they type in
 var searchType = document.querySelector("#search-select")
 
-var ingredient = inputEl.value || "lemon"
+var ingredient =  "lemon"
 var apiKey = "718caf0218dc49d49623438be5859ba7";
 var complexUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}`;
 var ingredientsSearch = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredient}`;
@@ -36,5 +36,26 @@ function runApi() {
 
 
 
-searchForm.addEventListener("submit", runApi)
+// searchForm.addEventListener("submit", runApi)
+
+
+
+let unsplashURL = "https://api.unsplash.com/search/photos?page=1&query=lemon&client_id=efCh77xmBOWOmOalj69JqwdI-oGi_pzWGDd7FlXj1Tw";
+
+function getPhotos(){
+
+    fetch(unsplashURL)
+    .then(function(data){
+        if(!data.ok){
+            console.log("issue try again");
+        }
+        return data.json();
+    })
+    .then(function(data){
+        console.log(data);
+        console.log(data.results[0].urls.thumb);
+    })
+}
+
+getPhotos();
 
