69717d5a24d14bcdc85d813577e28e3517f9efd7
DanialDevelops <danialchou101@gmail.com>
2023-04-23 21:41:21 -0400

    added param for api to redirect to recipe website

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -88,7 +88,7 @@ function getIngredientPhoto(data) {
 async function getRecipes(searchQuery) {
   // const apiKey = "718caf0218dc49d49623438be5859ba7"; // Rafee/Danials key
   const apiKey = "dd9b157a9b824a0282b9c785cadaebfc"
-  const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${searchQuery}`;
+  const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${searchQuery}&addRecipeInformation=true`;
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
